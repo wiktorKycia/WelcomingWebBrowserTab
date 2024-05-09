9d74977605a2a2a4f8a6ec4362bbb5f3bf465c83
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-05-09 16:41:49 +0200

    pull the searchbar querySelector out of the function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+let searchbar = document.querySelector("input#search");
+
 function search()
 {
-    let q = document.querySelector("input#search").value;
-    // window.location.href = "https://www.google.com/search?q=" + q + "";
+    let q = searchbar.value;
     window.open("https://www.google.com/search?q=" + q + "", "_blank").focus();
 }
