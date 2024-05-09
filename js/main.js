2f58e55ff414c9180d6c3d4c604d4e26c9dbfb89
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-05-09 16:39:58 +0200

    add js script to the html, in button: replace onlick with onclick

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 function search()
 {
     let q = document.querySelector("input#search").value;
+    // window.location.href = "https://www.google.com/search?q=" + q + "";
     window.open("https://www.google.com/search?q=" + q + "", "_blank").focus();
 }
