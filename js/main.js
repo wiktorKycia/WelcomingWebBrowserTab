00ed81f0ca4aa517f937fd1611e0a42345fd2ed2
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-06-21 19:58:41 +0200

    repair the gmail svg, then it works, but it is not readable

--- js/main.js
+++ js/main.js
@@ -12,8 +12,7 @@ function search()
     let q = searchbar.value;
     window.open("https://www.google.com/search?q=" + q + "", "_blank").focus();
 }
-const content = {
-    "sections": [
+const sections = [
         {
             "heading": "Google",
             "expansive": true,
@@ -26,7 +25,7 @@ const content = {
                 {
                     "name": "Gmail",
                     "url": "https://mail.google.com/mail/u/0/#inbox",
-                    "icon":"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" style=\"fill: rgba(0, 0, 0, 1);transform: ;msFilter:;\"><path d=\"M21.593 7.203a2.506 2.506 0 0 0-1.762-1.766C18.265 5.007 12 5 12 5s-6.264-.007-7.831.404a2.56 2.56 0 0 0-1.766 1.778c-.413 1.566-.417 4.814-.417 4.814s-.004 3.264.406 4.814c.23.857.905 1.534 1.763 1.765 1.582.43 7.83.437 7.83.437s6.265.007 7.831-.403a2.515 2.515 0 0 0 1.767-1.763c.414-1.565.417-4.812.417-4.812s.02-3.265-.407-4.831zM9.996 15.005l.005-6 5.207 3.005-5.212 2.995z\"></path></svg>"
+                    "icon":"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" style=\"fill: rgba(0, 0, 0, 1);transform: ;msFilter:;\"><path d=\"m18.73 5.41-1.28 1L12 10.46 6.55 6.37l-1.28-1A2 2 0 0 0 2 7.05v11.59A1.36 1.36 0 0 0 3.36 20h3.19v-7.72L12 16.37l5.45-4.09V20h3.19A1.36 1.36 0 0 0 22 18.64V7.05a2 2 0 0 0-3.27-1.64z\"></path></svg>"
                 },
                 {
                     "name": "Google Drive",
@@ -36,10 +35,10 @@ const content = {
             ]
         }
     ]
-}
+
 document.addEventListener("DOMContentLoaded", function () {
     
-    for(section of content)
+    for(section of sections)
     {
         let h2 = document.createElement('h2');
         h2.innerHTML = section.heading;
