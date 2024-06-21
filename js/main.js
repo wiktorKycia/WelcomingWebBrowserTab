5219d7605f0233ed804efa998d867f3cdb28f284
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-06-21 19:51:53 +0200

    tried to do it via json and fs module, but does not work

--- js/main.js
+++ js/main.js
@@ -12,3 +12,20 @@ function search()
     let q = searchbar.value;
     window.open("https://www.google.com/search?q=" + q + "", "_blank").focus();
 }
+import fs from 'fs';
+document.addEventListener("DOMContentLoaded", function () {
+    let content = fs.readdirSync('./json/sections.json');
+    for(section of content)
+    {
+        let h2 = document.createElement('h2');
+        h2.innerHTML = section.heading;
+        document.body.appendChild(h2);
+        for(content of section.contents)
+        {
+            let a = document.createElement('a');
+            a.href = content.url;
+            a.innerHTML = content.icon + "<br>" + content.name;
+            document.body.appendChild(a);
+        }
+    }
+});
